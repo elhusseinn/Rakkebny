60fb9af353960c9e366acfa4d4f2808ebd652a38
elhusseinn <elhusseinabdalaziz1@gmail.com>
2021-11-22 20:58:50 +0200

    added Database&system classes

---
+++ src/DataBase.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public abstract class DataBase {
+/*protected ArrayList <Customer> customers = new ArrayList<Customer>();
+protected ArrayList <Driver> drivers = new ArrayList<Driver>();
+protected ArrayList<Admin> admins = new ArrayList<Admin>();*/
+protected ArrayList <User> users = new ArrayList<User>();
+
+}

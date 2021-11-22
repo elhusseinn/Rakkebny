7980819cf1148ae42b2672a38e148b99acb22a52
loay fayez <84818419+leon019i@users.noreply.github.com>
2021-11-22 20:57:58 +0200

    loay_work

--- src/Ride.java
+++ src/Ride.java
@@ -1,7 +1,22 @@
 public class Ride {
     private String source;
     private String destination;
+    private Driver driver;
+    private Customer customer;
+    private RideReceipt rideReceipt;
     private double cost;
+
+    public RideReceipt getRideReceipt() { return rideReceipt; }
+
+    public void setRideReceipt(RideReceipt rideReceipt) { this.rideReceipt = rideReceipt; }
+
+    public Driver getDriver() { return driver; }
+
+    public void setDriver(Driver driver) { this.driver = driver; }
+
+    public Customer getCustomer() { return customer; }
+
+    public void setCustomer(Customer customer) { this.customer = customer; }
 
     public String getSource() {
         return source;
